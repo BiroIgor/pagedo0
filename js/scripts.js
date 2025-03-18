f2d54b5ddf947ce4da420ade6b553d423959d721
BiroIgor <igorkank@hotmail.com>
2025-03-18 16:21:19 -0300

    melhorias e funcionalidades, login2

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
-// This file is intentionally left blank.
+const users = [
+    { email: "igorkank@hotmail.com", password: "birozeez" },
+    //{ email: "admin@example.com", password: "aaaaa" }
+];
+
+function authenticate(event) {
+    event.preventDefault(); // Evita o recarregamento da página
+
+    const email = document.getElementById("login-email").value;
+    const password = document.getElementById("login-password").value;
+
+    // Verifica se as credenciais são válidas
+    const user = users.find(user => user.email === email && user.password === password);
+
+    if (user) {
+        alert("Login bem-sucedido!");
+        // Salva o estado de login no localStorage
+        localStorage.setItem("isLoggedIn", "true");
+        // Redireciona para a página inicial
+        window.location.href = "index.html";
+    } else {
+        alert("E-mail ou senha inválidos!");
+    }
+}
